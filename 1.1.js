5c8370bd23a95e0c610616f7a53edaa1d27d6a09
Luiz Barcelos <61237348+LuizBarcelos-top@users.noreply.github.com>
2022-08-30 17:43:42 -0300

    30.08

--- 1.1.js
+++ 1.1.js
@@ -1,1 +1,18 @@
-console.log("bunda");
+console.log("é...");
+const a = 10+30;
+console.log(a);
+
+
+console.log(parseInt(10)+parseInt(1000));
+
+const listaDeCidades = new Array(
+    'Tilambuco',
+    'Racoon',
+    'Alexandria'
+);
+
+// const sp = "São Paulo";
+// const rj = 'Rio de Janeiro';
+listaDeCidades.push('Gotham');
+
+console.log(listaDeCidades);
